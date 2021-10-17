63d74a061555501111553a5bfca6796cb3457adb
Prasanna Kumar Karanam <prasanna04k@gmail.com>
2021-10-16 21:38:17 -0500

    first change, added project

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public string Number { get; }
+
+        public string Name { get; set; }
+
+        public decimal Balance { get; }
+
+        public Class1(string name, decimal balance)
+        {
+            Name = name;
+            Balance = balance;
+            Number = "123456789";
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi there");
+            var varOne = Console.ReadLine();
+            var varTwo = Console.ReadLine();
+
+            Console.WriteLine(varOne + varTwo);
+        }
+    }
+}
+
